fc4d5028829aaa582404610bc3fbba190fd389db
Shin Chen <jiayuanchen@outlook.com>
2022-08-31 18:17:51 +1000

    Add files via upload
    fix OLS without fixed effect bugs

--- src/pystata.py
+++ src/pystata.py
@@ -2,34 +2,34 @@ import os
 import random
 import re
 import string
+import sys
 import numpy as np
 from IPython.core.display import display, HTML
 import pandas as pd
-from src.pyreg import OLS
+from pyreg import OLS
 
-# import configure
-import configparser
-
-config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
-config.read('config.ini')
-stata_path =  config.get('Directory', 'StataDir')
 
 class pystata(OLS):
-    '''Tunnel between python and Stata '''
-    def __init__(self, dat:pd.DataFrame, reg, cov_type, **kwargs):
+    def __init__(self, dat:pd.DataFrame, reg:str, cov_type:object, **kwargs):
         super().__init__(reg, cov_type, **kwargs)
-        self.data = dat #python pd.DataFrame data
-        self.inputDir = '' # Input Directory
-        self.outputDir = '' # output Directory
-        self.filename = 'temp.csv' # temporary filename for saved python pd.DataFrame data
+        # data: pandas DataFrame
+        self.data = dat
+
+        # Directory
+        self.inputDir = ''
+        self.outputDir = ''
+        self.filename = 'temp.csv'
         self.pydir = self.kwargs.get('dir')
 
-        # initialize the pystata
         self.set_pydir(self.pydir[0], self.pydir[1], self.pydir[2])
-        self._main_()
 
-    def _main_(self):
-        ''' Save data from python and read it on Stata'''
+        # main
+        self.main()
+
+    def main(self):
+        '''
+        THis is the main function
+        '''
         all_vars = self._get_vars()
         self.filename = self._savedf_(self.data, all_vars)
 
@@ -37,29 +37,39 @@ class pystata(OLS):
         self.do_script = self.read_script + self.do_script
 
     def set_pydir(self, workDir, _inputDir_, _outputDir_):
-        ''' Set the working directory and input and output directory'''
+        '''
+        Directory setter
+        '''
         self.inputDir = workDir
         self.inputDir = _inputDir_
         self.outputDir = _outputDir_
 
     def _load_(self):
-        ''' Load the data from python to Stata'''
+        '''
+        load csv file from the saved python pd.DataFrame
+        '''
         filename = f'"$input/temp_{self.filename}.csv"'
         comment = '* Load CSV saved from python \n'
         load_file = f'import delimited {filename}, clear \n'
         return comment + load_file
 
     def _get_vars(self):
-        ''' Get the variables from the regression specification'''
-        variables = self._parse(self.reg)
+        '''
+        When saving csv file that uses in Stata analysis, only save the required columns
+        Variables includes all independent variable, fixed effects, cluster variable
+        '''
+
+        # independent variables
+        variables = self._parse(self.reg, ret=True)
         unique_vars = list(set(variables))
 
-        # get cluster and fixed effects variables
+        # cluster variables
         try:
             cluster_vars = list(set(self.cluster_list))
         except TypeError:
             cluster_vars = []
 
+        # fixed effect variables
         try:
             fe_vars = list(set(self.fixed_effects.values()))
         except AttributeError or TypeError:
@@ -69,21 +79,27 @@ class pystata(OLS):
         return all_var
 
     def _save_(self):
-        ''' Save the data in Stata'''
+        '''
+        save data in .dta form
+        '''
         filename = f'"$output/temp/temp.dta"'
         comment = '* Save the dta file for regression \n'
         save_line = f'save {filename}, replace \n'
         return comment + save_line
 
     def _savedf_(self, df, cols):
-        ''' Save the python dataframe in a csv file'''
+        '''
+        save python DataFrame to inputdir for further Stata usage
+        '''
         filename = _randstr_()
         df[cols].to_csv(self.inputDir + f'temp_{filename}.csv', index=False)
         # print (f'Save pd.DataFrame to {self.inputDir}temp_{filename}.csv')
         return filename
 
     def save_dofile(self, _workDir_, _filename_):
-        ''' Save the do file'''
+        '''
+        save do script
+        '''
         filename = _filename_ + '.do'
         with open(_workDir_ + filename, 'w') as f:
             for row in self.do:
@@ -91,67 +107,80 @@ class pystata(OLS):
 
 
 class summary_col():
-    '''
-    Summary of the columns in the dataframe
-    '''
-    def __init__(self, reg_inputs: list):
-        global stata_path
-        self.reg_len = len(reg_inputs)
-        self.reg_dict = dict(zip(range(0, self.reg_len), reg_inputs))
-        self.data = reg_inputs
+    def __init__(self, reg_inputs:list):
+        # list of regression
+        self.reg_len = len(reg_inputs) # the number of regression
+        self.reg_dict = dict(zip(range(0, self.reg_len), reg_inputs)) # the map of regression
+        self.data = reg_inputs #
+
+        # Directory
         self.workDir = ''
         self.inputDir = ''
         self.outputDir = ''
         self.do_script = ''
-        self.macro = ''
-        self.name = ''
         self.logDir = ''
-        self.modelname = []
-        self.order = []
-        self.depvars = []
-        self.indep_vars = []
-        self.allvars = []
-        self.fx = []
-        self.outtype = 'tex'
-        self.title = ''
-        self.desc = ''
+
+        # Key table variables
+        self.macro = '' # Stata macro
+        self.name = 'table_temp' # save-as table name
+        self.modelname = [] # model name for each regression
+        self.order = [] # order of independent variables
+        self.depvars = [] # the list of dependent variables
+        self.indep_vars = [] # all independent variables
+        self.allvars = [] # all variables
+        self.fx = [] # fixed effects
+        self.outtype = 'tex' # table output type i.e., tex, html, xlsx
+        self.title = '' # table title caption
+        self.desc = '' # the table caption in description
         self.fx_space = {}
-        self.stata_path = stata_path
 
     def _main_(self):
+        # do file for each regression
         do_file = []
 
-        # collect fixed effects info before running regression
-        # for i, reg_input in enumerate(self.data):
+        # For each regression, collect all fixed effects info before running regression
+        # so that it can pre-set the all fixed effects in the regression statistics
         for reg_input in self.data:
             kwargs = self.get_fxcluster(reg_input)
-            fx_temp = kwargs['fx']
+            fx_temp = kwargs.get('fx')
             if fx_temp:
                 self.fx_space.update(kwargs['fx'])
 
-
-        # loop over each regression
+        # estimate each regression based on its specification
         for i, reg_input in enumerate(self.data):
             kwargs = self.get_fxcluster(reg_input)
+            # set working directory
             kwargs['dir'] = [self.workDir, self.inputDir, self.outputDir]
+
+            # set index for column i
             kwargs['column'] = i
             comments = self.comments(reg_input, **kwargs)
             kwargs['fx_space'] = list(self.fx_space.keys())
+
+            # check if covariance cluster is given
             if isinstance(reg_input[2], list):
-                temp= pystata(reg_input[0], reg_input[1], 'clustered', **kwargs)
+                # if given, estimated with clustered standard errors
+                temp = pystata(reg_input[0], reg_input[1], 'clustered', **kwargs)
             else:
-                temp= pystata(reg_input[0], reg_input[1], 'robust', **kwargs)
+                # else, estimated with robust standard errors
+                temp = pystata(reg_input[0], reg_input[1], 'robust', **kwargs)
 
             temp.do_script = comments + temp.do_script
             do_file.append(temp.do_script)
+            # append all dependent and independent variables for the sake of caption
             self.depvars.append(temp.dep_var)
             self.indep_vars.extend(temp.indep_vars)
 
+        # drop duplicated variables
         self.allvars = list(set(self.depvars + self.indep_vars))
+
+        # format do script and save
         self.do_script = self.macro + '\n'.join(do_file)
         self.add_header
         self._summary_()
         self.save_dofile(self.outputDir, self.name)
+
+        # initiate blank results text
         self.results = ''
 
     # generate comments
@@ -196,7 +225,6 @@ class summary_col():
 
     @property
     def add_header(self):
-        ''' Add header to the do file'''
         header = '*Please clean all the file in the output directory before running the code\n' \
                  'clear all\n' \
                  'macro drop _all\n' \
@@ -204,7 +232,6 @@ class summary_col():
         self.do_script = header + self.do_script
 
     def set_dir(self, workDir, _inputsubDir_='input', _outputsubDir_='output', log='log'):
-        ''' Set the directory for Stata'''
         globaldir = f'"{workDir}"'
         inputdir = f'"$dir/{_inputsubDir_}"'
         outputdir = f'"$dir/{_outputsubDir_}"'
@@ -244,21 +271,26 @@ class summary_col():
         except AttributeError or TypeError:
             fe_vars = []
         all_var = unique_vars + cluster_vars + fe_vars
-
+        # comments_head = '*Only keep the variables that is relevant to the regressions \n'
+        # keep_line = 'keep ' + ' '.join(all_var)  + '\n'
+        # comments_end = '*End of Keep \n'
 
         return all_var
 
-
     def run_do(self):
-        ''' Run the do file in Stata'''
+        if sys.platform == 'win32':
+            # stata_path = 'C:\Program Files (x86)\Stata14\StataMP-64.exe'
+            stata_path = 'C:\Program Files (x86)\Stata14\StataMP-64.exe'
+        else:
+            stata_path = '/usr/local/stata14/stata-mp'
+
         filename = self.outputDir + f'{self.name}'
         os.chdir(self.logDir)
-        os.system(f'{self.stata_path} -e do {filename}')
+        os.system(f'{stata_path} -e do {filename}')
         self.cleancache
         self.results = open(self.outputDir + f'{self.name}.{self.outtype}').read()
 
     def _summary_(self):
-        ''' Stata summarize regression results'''
         models = [f'"{model}"' for model in self.modelname] if self.modelname else []
         modelname = ' mtitles (' + ' '.join(models) + ')' if models else ''
 
@@ -272,13 +304,12 @@ class summary_col():
         _fx_label = ' '.join([f'"{k}"' for k in self.fx_space.keys()])
         _format_stata = ' '.join(['0' for i in range(0, len(self.fx_space.keys()))])
         self.summary = f'esttab {reg_list} using "$output/{self.name}.{self.outtype}", replace ' \
-                       'star(* 0.10 ** 0.05 *** 0.01) stat(' + _fx_stata + ' N r2_a tcov, ' \
-                       'fmt('+ _format_stata + ' 0 3 0) label(' + _fx_label +  \
+                       'star(* 0.10 ** 0.05 *** 0.01) stat(' + _fx_stata + ' N r2_within tcov, ' \
+                                                                           'fmt(' + _format_stata + ' 0 3 0) label(' + _fx_label + \
                        ' "Observations" "Adjusted R2" "SE Type")) noconstant' + modelname + orderlist.lower()
         self.do_script = self.do_script + comment + self.summary + '\n exit, STATA clear'
 
     def _readhtml_(self, columns=[]):
-        ''' Read the html file'''
         self.results = open(self.outputDir + f'{self.name}.html').read()
         ind = self.results.find('td colspan=')
         ncol = findint(self.results[ind + 11:ind + 13])
@@ -290,7 +321,6 @@ class summary_col():
         display(HTML(self.results))
 
     def _readtex_(self, columns=[]):
-        ''' Read the tex file'''
         self.results = open(self.outputDir + f'{self.name}.tex').read()
         if columns:
             self.rename(columns)
@@ -298,14 +328,14 @@ class summary_col():
         return self.results
 
     def rename(self, dep_dict):
-        ''' Rename the independent variables'''
         lower_dict = dict((k.replace("_", "\_").lower(), v) for k, v in dep_dict.items())
         for oldname in lower_dict.keys():
+            # oldname = oldname.replace("_", "\_").lower()
             self.results = self.results.replace(oldname, lower_dict[oldname][0])
-
+        # for oldname in columns.keys():
+        #     self.results = self.results.replace(oldname, columns[oldname])
 
     def format(self, ratio=1):
-        ''' Format the tex file, this is customized for my project (will be removed)'''
         table_start = self.results.find('\\begin{tabular}')
         self.results = self.results[:table_start] + '\n \\begin{adjustbox}{width=' + str(
             ratio) + '\\textwidth} \n' + self.results[table_start:]
@@ -320,9 +350,7 @@ class summary_col():
 
         self.results = header + self.results + caption + footer
 
-
     def add_avg(self, avg_list, name='Average'):
-        ''' Add mean in first row, this is customized for my project (will be removed)'''
         ind = self.results.find('\\hline\\hline') + 14
         ind_add = self.results[ind::].find('\\hline') + 8
         avg = '&' + '&'.join([str(x) for x in avg_list])
@@ -333,7 +361,6 @@ class summary_col():
                                                                                                  ind + ind_add - 1::]
 
     def add_group(self, group):
-        ''' add subgroup for columns, this is customized for my project (will be removed)'''
         tab_ind = self.results.find('\\begin{tabular}{') + 16
         tab_mod = '@{\extracolsep{4pt}}'
         self.results = self.results[:tab_ind] + tab_mod + self.results[tab_ind::]
@@ -356,7 +383,6 @@ class summary_col():
 
     @property
     def cleancache(self):
-        ''' Clean the cache'''
         temp_files = [temp for temp in os.listdir(self.inputDir) if 'temp_' in temp]
         os.chdir(self.inputDir)
         for item in temp_files:
@@ -366,7 +392,6 @@ class summary_col():
                 print(f'{detail} : {item}')
 
     def save(self, **kwargs):
-        ''' Save the tex file'''
         tempdir = kwargs.get('outputDir')
         tempname = kwargs.get('name')
         outputDir = tempdir if tempdir else self.outputDir
@@ -385,7 +410,7 @@ class summary_col():
         printout = easy_print(self.results)
         return printout
 
-    def print(self):
+    def show(self):
         '''
         Print tex table before complie in latex
         It is useful in ipython but not in jupyter
@@ -406,7 +431,9 @@ def folder_space(_workDir_: str, subfolder_name: str, local_folder: bool = True)
         workDir = CurrDir if local_folder else CurrDir + 'New folder/'
     else:
         workDir = _workDir_ if _workDir_[-1] == '/' else _workDir_ + '/'
-
+    #     filename = workDir + subfolder_name + '/'
+    # else:
+    #     filename = filename_temp
 
     filename = workDir + subfolder_name + '/'
 
@@ -455,10 +482,10 @@ def sread(fname, nlines):
                 print(line)
 
 
-def _cut_text_(tex, keyword, head = True):
+def _cut_text_(tex, keyword, head=True):
     if len(keyword) == 2:
         s_ind = tex.find(keyword[0])
-        e_ind = tex[s_ind+ len(keyword[0])::].find(keyword[1]) + s_ind + len(keyword[0])
+        e_ind = tex[s_ind + len(keyword[0])::].find(keyword[1]) + s_ind + len(keyword[0])
         if s_ind == -1:
             s_ind == 0
         if e_ind != -1:
@@ -482,11 +509,11 @@ def _cut_text_(tex, keyword, head = True):
                 return tex
 
 
-def _clean_coefficients(coefficients, val_star = True):
-    temp = [c.replace(" ","") for c in coefficients]
-    coeff_temp = [_cut_text_(c,['\\sym'],head = False) for c in temp]
+def _clean_coefficients(coefficients, val_star=True):
+    temp = [c.replace(" ", "") for c in coefficients]
+    coeff_temp = [_cut_text_(c, ['\\sym'], head=False) for c in temp]
     if val_star:
-        stars = [_cut_text_(c,['\\sym{','}']) for c in temp]
+        stars = [_cut_text_(c, ['\\sym{', '}']) for c in temp]
         coeff = [f'{coeff_temp[i]}{stars[i]}' for i in range(len(coeff_temp))]
     else:
         coeff = coeff_temp
@@ -499,8 +526,9 @@ def easy_print(tex):
 
     # from table start and end
     ind = [i for i, x in enumerate(lines) if x == '\\hline\\hline']
-    tables = lines[ind[0]+1:ind[1]]
+    tables = lines[ind[0] + 1:ind[1]]
     return tables
+
 
 def getmodel(tex):
     # split the tex by lines
@@ -508,49 +536,49 @@ def getmodel(tex):
 
     # from table start and end
     ind = [i for i, x in enumerate(lines) if x == '\\hline\\hline']
-    tables = lines[ind[0]+1:ind[1]]
+    tables = lines[ind[0] + 1:ind[1]]
 
     # find model title
     models_ind = [i for i, x in enumerate(tables) if x == '\\hline']
     temp = tables[:models_ind[0]]
 
     models_temp = [x.strip(' \\').split('&') for x in temp]
-    model_number = [_cut_text_(x,['{(',')}'] )
+    model_number = [_cut_text_(x, ['{(', ')}'])
                     for x in models_temp[0] if x != '']
-    model_name = [_cut_text_(x,['c}{','}'] )
-                    for x in models_temp[1] if x != '']
+    model_name = [_cut_text_(x, ['c}{', '}'])
+                  for x in models_temp[1] if x != '']
     models = [f'{model_name[i]} ({model_number[i]})' for i in range(len(model_name))]
 
     # get table info
-    rows = [x.strip('\\\\') for x in tables[models_ind[0]:models_ind[1]] if x not in ['\\hline','[1em]']]
+    rows = [x.strip('\\\\') for x in tables[models_ind[0]:models_ind[1]] if x not in ['\\hline', '[1em]']]
     coefficients_list_temp = [rows[i].split("&")[1::] for i in range(len(rows))]
     coefficients_list = [_clean_coefficients(c) for c in coefficients_list_temp]
 
-    vars =  [rows[i].split("&")[0].strip(' \\') for i in range(len(rows))]
-
+    vars = [rows[i].split("&")[0].strip(' \\') for i in range(len(rows))]
 
     ## Statistics
-    rows = [x.strip('\\\\') for x in tables[models_ind[1]::] if x not in ['\\hline','[1em]']]
+    rows = [x.strip('\\\\') for x in tables[models_ind[1]::] if x not in ['\\hline', '[1em]']]
     stat_list_temp = [rows[i].split("&")[1::] for i in range(len(rows))]
     stat_list = [_clean_coefficients(c) for c in stat_list_temp]
 
-    stats_name =  [rows[i].split("&")[0].strip(' \\') for i in range(len(rows))]
+    stats_name = [rows[i].split("&")[0].strip(' \\') for i in range(len(rows))]
 
     # put together
-    print = pd.DataFrame(coefficients_list, columns = models)
+    print = pd.DataFrame(coefficients_list, columns=models)
     print['Var'] = vars
 
     # add line between table and statistics
-    s_line = {f'{c}':'---' for c in print.columns.tolist()}
-    print = print.append(s_line, ignore_index = True)
+    s_line = {f'{c}': '---' for c in print.columns.tolist()}
+    print = print.append(s_line, ignore_index=True)
 
-    stat = pd.DataFrame(stat_list, columns = models)
+    stat = pd.DataFrame(stat_list, columns=models)
     stat['Var'] = stats_name
 
     print = print.append(stat)
     print[['Var'] + models]
 
     return print[['Var'] + models]
+
 
 def findint(s):
     return int(re.search(r'\d+', s).group())
